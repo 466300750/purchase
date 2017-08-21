ed96a5f775503fb18e1b2912f91a8ccb2224023e
YueSu <yuesu@thoughtworks.com>
2017-08-21 20:32:29 +0800

    pass the tesst

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var Purchase = artifacts.require("./Purchase.sol");
 
-module.exports = function(deployer) {
-    deployer.deploy(Purchase);
+module.exports = function(deployer, env, accounts) {
+    deployer.deploy(Purchase, {from: accounts[0], value: 2});
 };
